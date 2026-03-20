4efc90a81db686e56aa421546cfc3b9e06b4e5c9
kttk <katanotaka@gmail.com>
2026-03-21 01:47:15 +0900

    reference and changable reference

--- reference/src/main.rs
+++ reference/src/main.rs
@@ -1,12 +1,29 @@
 fn main() {
-    let s1 = String::from("hello");
-
-    let len = calculate_length(&s1);
+    let mut s1 = String::from("hello");
+    let mut len = calculate_length(&mut s1);
 
     // '{}'の長さは、{}です
-    println!("The length of '{}' is {}.", s1, len);
+    println!("The length of '{s1}' is {len}." );
+    
+    change(&mut s1);
+    len = calculate_length(&mut s1);
+    
+    println!("The length of '{s1}' is {len}" );
+        
 }
 
-fn calculate_length(s: &String) -> usize {
+fn calculate_length(s: &mut String) -> usize {
     s.len()
 }
+
+fn change(s: &mut String){
+    s.push_str(", world.");
+}
+
+
+//fn dangle() -> &String { // dangleはStringへの参照を返す
+//    let s = String::from("hello"); // sは新しいString
+//    &s // String sへの参照を返す
+// ここで、sはスコープを抜け、ドロップされる。そのメモリは消される。
+// 危険だ
+
