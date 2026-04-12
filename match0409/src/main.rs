aa18783badf167ba63e08c73478c5f1374081b66
kttk <katanotaka@gmail.com>
2026-04-13 08:43:27 +0900

    match done

---
+++ match0409/src/main.rs
@@ -0,0 +1,31 @@
+
+
+fn main() {
+    println!("Hello, world!");
+    let val = 
+        value_in_cents(Coin::Nickel);
+    println!("Hello, {} world!", val);
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel =>{
+            println!("nickel get");
+            5},
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+
+
+
+
